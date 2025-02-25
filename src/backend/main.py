4ae4b9510c0f2688489c72dfe074cbe1652773d4
Tim Pollmann <84510339+tim-pollmann@users.noreply.github.com>
2025-02-25 13:23:24 +0000

    Backup changes

--- src/backend/main.py
+++ src/backend/main.py
@@ -9,10 +9,29 @@ import string
 
 app = FastAPI()
 
+short_idx = -1
+short_to_long = {}
+SYMBOL_LIST = string.ascii_letters + string.digits
 # TODO: implement some kind of storage for the URLs and their associated short codes
 
 class UrlRequest(BaseModel):
     url: str
+
+
+def encode_base62(num: int) -> str:   
+    characters = string.digits + string.ascii_letters  # 62 characters
+    base = len(characters)
+    encoded = []
+    
+    if num == 0:
+        return characters[0]
+    
+    while num > 0:
+        num, remainder = divmod(num, base)
+        encoded.append(characters[remainder])
+    
+    return ''.join(reversed(encoded))
+
 
 def generate_short_code(length: int = 6) -> str:
     return ''.join(random.choices(string.ascii_letters + string.digits, k=length))
@@ -21,15 +40,17 @@ def generate_short_code(length: int = 6) -> str:
 def shorten_url(request: UrlRequest):
     # TODO: implement this function
     # Should store url which is contained in request, generate a (random?) associated short code and return that short code
-    short_code = ""
+    global short_idx
+    short_idx += 1
+    short_code = encode_base62(short_idx)
+    short_to_long[short_code] = request.url
     return {short_code}
 
 @app.get("/get-long-url/{short_code}")
 def get_long_url(short_code: str):
-    # TODO: implement this function
-    # Should take the short_code to fetch the associated long URL 
-    # Should return HTTP 404 (not found) if the short_code is not found
-    long_url = ""
+    long_url = short_to_long.get(short_code, -1)
+    if long_url == -1:
+        raise HTTPException(status_code=404, detail="Short code not found")
     return {long_url}
 
 @app.get("/get-qr-code/{url}")
@@ -37,8 +58,15 @@ def get_qr_code(url: str):
     # TODO: implement this function
     # Hint: You can use the qrcode library to generate QR codes. 
     # Take a look at the documentation here: https://pypi.org/project/qrcode/
-    encoded_string = ""
-    # return {"image_base64": f"data:image/png;base64,{encoded_string}"}
+    img = qrcode.make(url)
+    # encode img to base64
+    # img.save("some_file.png")
+    byteIO = io.BytesIO()
+    img.save(byteIO, format='PNG')
+    byteArr = byteIO.getvalue()
+    encoded_string = base64.b64encode(byteArr).decode('utf-8')
+
+    return {"image_base64": f"data:image/png;base64,{encoded_string}"}
 
 # Returns simple string message
 @app.get("/example")
